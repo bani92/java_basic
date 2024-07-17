9809fdb716fd2b474c9e6b3f8c985be14366b5a4
bani <dalllo9851@gmail.com>
2024-07-17 21:46:01 +0900

    자바 Stream 2문제(8) - medium

--- src/main/Problem/medium/Problem34.java
+++ src/main/Problem/medium/Problem34.java
@@ -1,5 +1,6 @@
 package Problem.medium;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -30,6 +31,38 @@ public class Problem34 {
         // 여기에 코드 작성
 
         return IntStream.range(0, numbers.size() -1).allMatch(num -> numbers.get(num) < numbers.get(num + 1));
+    }
+
+    /**
+     * 주어진 문자열 리스트에서 각 문자열의 첫 글자를 대문자로 변환하고,
+     * 나머지 글자는 소문자로 변환한 새로운 리스트를 반환합니다.
+     *
+     * @param strings 문자열 리스트
+     * @return 변환된 문자열 리스트
+     */
+    public static List<String> capitalizeFirstLetter(List<String> strings) {
+        // 여기에 코드 작성
+        return strings.stream().map(word -> word.replaceFirst(word, word.substring(0, 1).toUpperCase()) + word.substring(1).toLowerCase()).toList();
+    }
+
+    /**
+     * 주어진 정수 리스트에서 합이 10을 초과하는 최초의 연속된 부분 리스트를 반환합니다.
+     * 예를 들어, [1, 2, 3, 5, 4]의 경우 [3, 5, 4]가 해당합니다.
+     *
+     * @param numbers 정수 리스트
+     * @return 합이 10을 초과하는 최초의 연속된 부분 리스트
+     */
+    public static List<Integer> findFirstSublistWithSumOverTen(List<Integer> numbers) {
+        // 여기에 코드 작성
+        int[] sum = {0};
+        return IntStream.range(0,numbers.size())
+                .boxed()
+                .flatMap(start -> IntStream.range(start, numbers.size()).mapToObj(end -> numbers.subList(start, end + 1)))
+                .peek(sublist -> sum[0] = sublist.stream().mapToInt(Integer::intValue).sum())
+                .filter(sublist -> sum[0] > 10)
+                .findFirst()
+                .orElse(Collections.emptyList());
+
     }
 }
 
--- src/test/java/Problem/medium/Problem34Test.java
+++ src/test/java/Problem/medium/Problem34Test.java
@@ -18,4 +18,14 @@ class Problem34Test {
         assertTrue(Problem34.isSequentiallyIncreasing(Arrays.asList(1, 2, 3, 4)));
         assertFalse(Problem34.isSequentiallyIncreasing(Arrays.asList(1, 3, 2, 4)));
     }
+
+    @Test
+    public void testCapitalizeFirstLetter() {
+        assertEquals(Arrays.asList("Apple", "Banana", "Cherry"), Problem34.capitalizeFirstLetter(Arrays.asList("apple", "banana", "cherry")));
+    }
+
+    @Test
+    public void testFindFirstSublistWithSumOverTen() {
+        assertEquals(Arrays.asList(1, 2, 3, 5), Problem34.findFirstSublistWithSumOverTen(Arrays.asList(1, 2, 3, 5, 4)));
+    }
 }
